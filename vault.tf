c1b8869fb74ee6a684353f26c103fc599aa0f60d
jeff <jeffrey.spiers@mail.mcgill.ca>
2020-10-30 21:46:16 -0600

    using remote chart instead of submodule

--- vault.tf
+++ vault.tf
@@ -1,7 +1,10 @@
 resource "helm_release" "vault" {
   count     = data.terraform_remote_state.cluster.outputs.enable_consul_and_vault ? 1 : 0
+  repository = "https://helm.releases.hashicorp.com"
+  chart     = "vault"
   name      = "${data.terraform_remote_state.consul.outputs.release_name}-vault"
-  chart     = "${path.module}/vault-helm"
+  chart      = "vault"
+  # version    = "0.25"
   namespace = data.terraform_remote_state.consul.outputs.namespace
 
   set {
