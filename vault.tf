0a52862f147a3d57a602d6475c1973d6e0f0f7ab
jeff <jeffrey.spiers@mail.mcgill.ca>
2020-10-30 21:49:53 -0600

    removed superfluous 'chart' in vault.tf

--- vault.tf
+++ vault.tf
@@ -1,11 +1,10 @@
 resource "helm_release" "vault" {
-  count     = data.terraform_remote_state.cluster.outputs.enable_consul_and_vault ? 1 : 0
+  count      = data.terraform_remote_state.cluster.outputs.enable_consul_and_vault ? 1 : 0
   repository = "https://helm.releases.hashicorp.com"
-  chart     = "vault"
-  name      = "${data.terraform_remote_state.consul.outputs.release_name}-vault"
   chart      = "vault"
   # version    = "0.25"
   namespace = data.terraform_remote_state.consul.outputs.namespace
+  name      = "${data.terraform_remote_state.consul.outputs.release_name}-vault"
 
   set {
     name  = "server.ha.enabled"
